0e8ba64f4aeb0fc3f846686dd396fff8e9a2087c
amydiaz1 <amydiaz1@mail.weber.edu>
2025-04-25 15:36:41 -0600

    dog

--- dog.py
+++ dog.py
@@ -4,11 +4,16 @@ class Dog:
     """
     # TODO: Initialize the Dog class and add the breed attribute.
     # The constructor should accept name, species, and breed as parameters.
-    
+    def __init__(self, name, species, breed):
+        super().__init__(name, species)
+        self.breed = breed
     # TODO: Override the __str__ method to include the breed.
     # Example output:
     # Kingdom: 'kingdom attribute', Name: 'name attribute', Species: 'species attribute', Breed: 'breed attribute'
-    
+    def __str__(self):
+        return (f"Kingdom: 'Animalia', Name: '{self.name}', Species: '{self.species}, Breed: {self.breed}'")
     # TODO: Add a method for the dog to make a specific sound. 
     # Call the method `speak` and make it output a specific message like 
     # "The dog barks.
+    def speak(self):
+        return ("The dog barks.")
